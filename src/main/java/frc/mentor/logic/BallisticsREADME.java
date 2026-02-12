c8b5ee37c35fc9fdbc245636f28d390ebc530ecc
Fabien <fabi.gaudreau@gmail.com>
2026-02-12 10:08:48 -0500

    Ajout de la structure des fonctions mentor

---
+++ src/main/java/frc/mentor/logic/BallisticsREADME.java
@@ -0,0 +1,17 @@
+package frc.mentor.logic;
+
+/**
+ * API exposée aux jeunes (shoot on the move)
+ *
+ * - ShooterOutput BallisticsDynamic.update(ShooterInputs in)
+ * - ShooterSetpoints BallisticsDynamic.getShootingParamsDynamic(ShooterInputs in)
+ * - double BallisticsDynamic.estimateTimeOfFlight(double distanceM, double rpm, double hoodDeg)
+ * - double BallisticsDynamic.calculateYawRealTime(ShooterInputs in)
+ * - double BallisticsDynamic.calculateHoodDegRealTime(ShooterInputs in)
+ * - double BallisticsDynamic.getConfidence(ShooterInputs in) // 0..1
+ */
+
+public final class BallisticsREADME {
+    private BallisticsREADME() {
+    }
+}
